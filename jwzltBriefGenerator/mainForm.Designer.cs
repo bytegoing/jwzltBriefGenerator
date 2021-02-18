fd09ebfeb0e856dee4c4be6fbf494c3bc36c3ae0
BYTEGOING <bytegoing@outlook.com>
2021-02-18 21:09:20 +0800

    Modified: Design of mainForm

--- jwzltBriefGenerator/mainForm.Designer.cs
+++ jwzltBriefGenerator/mainForm.Designer.cs
@@ -55,14 +55,12 @@ namespace jwzltBriefGenerator
             this.editOriginDataButton = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.exportOriginData = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // userExportBriefDataButton
             // 
-            this.userExportBriefDataButton.Location = new System.Drawing.Point(362, 156);
+            this.userExportBriefDataButton.Location = new System.Drawing.Point(359, 142);
             this.userExportBriefDataButton.Name = "userExportBriefDataButton";
             this.userExportBriefDataButton.Size = new System.Drawing.Size(168, 38);
             this.userExportBriefDataButton.TabIndex = 11;
@@ -72,7 +70,7 @@ namespace jwzltBriefGenerator
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(112, 25);
+            this.username.Location = new System.Drawing.Point(118, 9);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(100, 25);
             this.username.TabIndex = 18;
@@ -80,7 +78,7 @@ namespace jwzltBriefGenerator
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 30);
+            this.label1.Location = new System.Drawing.Point(14, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 15);
             this.label1.TabIndex = 11;
@@ -89,7 +87,7 @@ namespace jwzltBriefGenerator
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(559, 132);
+            this.label3.Location = new System.Drawing.Point(566, 115);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(127, 15);
             this.label3.TabIndex = 17;
@@ -98,7 +96,7 @@ namespace jwzltBriefGenerator
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(445, 132);
+            this.label2.Location = new System.Drawing.Point(452, 115);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 15);
             this.label2.TabIndex = 16;
@@ -107,7 +105,7 @@ namespace jwzltBriefGenerator
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(335, 110);
+            this.label12.Location = new System.Drawing.Point(342, 93);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(82, 15);
             this.label12.TabIndex = 11;
@@ -116,7 +114,7 @@ namespace jwzltBriefGenerator
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(642, 110);
+            this.label11.Location = new System.Drawing.Point(649, 93);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(22, 15);
             this.label11.TabIndex = 15;
@@ -125,7 +123,7 @@ namespace jwzltBriefGenerator
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(526, 110);
+            this.label10.Location = new System.Drawing.Point(533, 93);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(52, 15);
             this.label10.TabIndex = 14;
@@ -134,7 +132,7 @@ namespace jwzltBriefGenerator
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(424, 110);
+            this.label9.Location = new System.Drawing.Point(431, 93);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(37, 15);
             this.label9.TabIndex = 13;
@@ -142,14 +140,14 @@ namespace jwzltBriefGenerator
             // 
             // userProcessToText
             // 
-            this.userProcessToText.Location = new System.Drawing.Point(583, 104);
+            this.userProcessToText.Location = new System.Drawing.Point(590, 87);
             this.userProcessToText.Name = "userProcessToText";
             this.userProcessToText.Size = new System.Drawing.Size(53, 25);
             this.userProcessToText.TabIndex = 12;
             // 
             // userProcessFromText
             // 
-            this.userProcessFromText.Location = new System.Drawing.Point(467, 104);
+            this.userProcessFromText.Location = new System.Drawing.Point(474, 87);
             this.userProcessFromText.Name = "userProcessFromText";
             this.userProcessFromText.Size = new System.Drawing.Size(53, 25);
             this.userProcessFromText.TabIndex = 11;
@@ -157,7 +155,7 @@ namespace jwzltBriefGenerator
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 107);
+            this.label8.Location = new System.Drawing.Point(15, 90);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(83, 15);
             this.label8.TabIndex = 10;
@@ -167,14 +165,14 @@ namespace jwzltBriefGenerator
             // 
             this.userDepartmentCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.userDepartmentCombo.FormattingEnabled = true;
-            this.userDepartmentCombo.Location = new System.Drawing.Point(97, 104);
+            this.userDepartmentCombo.Location = new System.Drawing.Point(104, 87);
             this.userDepartmentCombo.Name = "userDepartmentCombo";
             this.userDepartmentCombo.Size = new System.Drawing.Size(209, 23);
             this.userDepartmentCombo.TabIndex = 9;
             // 
             // userChooseFileButton
             // 
-            this.userChooseFileButton.Location = new System.Drawing.Point(675, 61);
+            this.userChooseFileButton.Location = new System.Drawing.Point(681, 45);
             this.userChooseFileButton.Name = "userChooseFileButton";
             this.userChooseFileButton.Size = new System.Drawing.Size(45, 26);
             this.userChooseFileButton.TabIndex = 8;
@@ -184,7 +182,7 @@ namespace jwzltBriefGenerator
             // 
             // userFilename
             // 
-            this.userFilename.Location = new System.Drawing.Point(166, 62);
+            this.userFilename.Location = new System.Drawing.Point(172, 46);
             this.userFilename.Name = "userFilename";
             this.userFilename.ReadOnly = true;
             this.userFilename.Size = new System.Drawing.Size(504, 25);
@@ -193,7 +191,7 @@ namespace jwzltBriefGenerator
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 67);
+            this.label7.Location = new System.Drawing.Point(14, 51);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(158, 15);
             this.label7.TabIndex = 7;
@@ -201,7 +199,7 @@ namespace jwzltBriefGenerator
             // 
             // userExportBriefDocButton
             // 
-            this.userExportBriefDocButton.Location = new System.Drawing.Point(174, 156);
+            this.userExportBriefDocButton.Location = new System.Drawing.Point(172, 142);
             this.userExportBriefDocButton.Name = "userExportBriefDocButton";
             this.userExportBriefDocButton.Size = new System.Drawing.Size(168, 38);
             this.userExportBriefDocButton.TabIndex = 6;
@@ -211,7 +209,7 @@ namespace jwzltBriefGenerator
             // 
             // userProcessBriefButton
             // 
-            this.userProcessBriefButton.Location = new System.Drawing.Point(11, 156);
+            this.userProcessBriefButton.Location = new System.Drawing.Point(17, 142);
             this.userProcessBriefButton.Name = "userProcessBriefButton";
             this.userProcessBriefButton.Size = new System.Drawing.Size(144, 38);
             this.userProcessBriefButton.TabIndex = 5;
@@ -221,7 +219,7 @@ namespace jwzltBriefGenerator
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 335);
+            this.button1.Location = new System.Drawing.Point(182, 285);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 38);
             this.button1.TabIndex = 18;
@@ -230,7 +228,7 @@ namespace jwzltBriefGenerator
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(393, 291);
+            this.button12.Location = new System.Drawing.Point(392, 241);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(99, 38);
             this.button12.TabIndex = 17;
@@ -239,7 +237,7 @@ namespace jwzltBriefGenerator
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(288, 291);
+            this.button11.Location = new System.Drawing.Point(287, 241);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(99, 38);
             this.button11.TabIndex = 16;
@@ -248,7 +246,7 @@ namespace jwzltBriefGenerator
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(183, 291);
+            this.button10.Location = new System.Drawing.Point(182, 241);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(99, 38);
             this.button10.TabIndex = 15;
@@ -257,7 +255,7 @@ namespace jwzltBriefGenerator
             // 
             // formatDataButton
             // 
-            this.formatDataButton.Location = new System.Drawing.Point(288, 247);
+            this.formatDataButton.Location = new System.Drawing.Point(287, 197);
             this.formatDataButton.Name = "formatDataButton";
             this.formatDataButton.Size = new System.Drawing.Size(99, 38);
             this.formatDataButton.TabIndex = 14;
@@ -266,17 +264,17 @@ namespace jwzltBriefGenerator
             // 
             // editOriginDataButton
             // 
-            this.editOriginDataButton.Location = new System.Drawing.Point(183, 247);
+            this.editOriginDataButton.Location = new System.Drawing.Point(182, 197);
             this.editOriginDataButton.Name = "editOriginDataButton";
             this.editOriginDataButton.Size = new System.Drawing.Size(99, 38);
             this.editOriginDataButton.TabIndex = 13;
-            this.editOriginDataButton.Text = "1.原始数据编辑";
+            this.editOriginDataButton.Text = "1.原始数据裁剪";
             this.editOriginDataButton.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 303);
+            this.label5.Location = new System.Drawing.Point(21, 253);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(83, 15);
             this.label5.TabIndex = 12;
@@ -285,42 +283,15 @@ namespace jwzltBriefGenerator
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 259);
+            this.label4.Location = new System.Drawing.Point(21, 209);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(143, 15);
             this.label4.TabIndex = 12;
             this.label4.Text = "A.简报原始数据处理";
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.userExportBriefDataButton);
-            this.groupBox1.Controls.Add(this.label11);
-            this.groupBox1.Controls.Add(this.userFilename);
-            this.groupBox1.Controls.Add(this.username);
-            this.groupBox1.Controls.Add(this.userChooseFileButton);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.label12);
-            this.groupBox1.Controls.Add(this.userProcessFromText);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.userProcessBriefButton);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.userProcessToText);
-            this.groupBox1.Controls.Add(this.userDepartmentCombo);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.userExportBriefDocButton);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(738, 215);
-            this.groupBox1.TabIndex = 19;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "简报常用功能";
-            // 
             // exportOriginData
             // 
-            this.exportOriginData.Location = new System.Drawing.Point(393, 247);
+            this.exportOriginData.Location = new System.Drawing.Point(392, 197);
             this.exportOriginData.Name = "exportOriginData";
             this.exportOriginData.Size = new System.Drawing.Size(99, 38);
             this.exportOriginData.TabIndex = 20;
@@ -332,23 +303,38 @@ namespace jwzltBriefGenerator
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(762, 504);
+            this.Controls.Add(this.userExportBriefDataButton);
             this.Controls.Add(this.exportOriginData);
+            this.Controls.Add(this.userExportBriefDocButton);
+            this.Controls.Add(this.userProcessBriefButton);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.userFilename);
+            this.Controls.Add(this.userProcessFromText);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.userChooseFileButton);
+            this.Controls.Add(this.userProcessToText);
             this.Controls.Add(this.button12);
+            this.Controls.Add(this.userDepartmentCombo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button11);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.editOriginDataButton);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.formatDataButton);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
             this.Name = "mainForm";
             this.Text = "NEPU教务助理团简报助手";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainForm_FormClosed);
             this.Load += new System.EventHandler(this.mainForm_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -381,7 +367,6 @@ namespace jwzltBriefGenerator
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button userExportBriefDataButton;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button exportOriginData;
     }
 }
